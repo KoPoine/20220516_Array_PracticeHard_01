6f68b9a39918cfab6b5f554b23ac0e0607ffa331
조경진 <cho881020@gmail.com>
2022-05-16 16:03:30 +0900

    1. 정수 6개 입력받기
     - 범위가 올바른지
    
     - 중복되는 내용이 없는지

---
+++ src/codes/MainDrive.java
@@ -0,0 +1,34 @@
+package codes;
+
+import java.util.Scanner;
+
+public class MainDrive {
+	public static void main(String[] args) {
+		Scanner myScanner = new Scanner(System.in);
+		
+		int[] myNumbers = new int [6];
+		
+		for (int i = 0; i < 6; i++) {
+			
+			while (true) {
+				System.out.print((i+1) + "번째 숫자 : ");
+				int inputNum = myScanner.nextInt();
+				
+				boolean isRangeOk = 1 <= inputNum && inputNum <= 45;
+				
+				boolean isRepeat = false;
+				
+				for (int num : myNumbers) {
+					if (num == inputNum) {
+						isRepeat = true;
+					}
+				}
+				
+				if (isRangeOk && !isRepeat) {
+					myNumbers[i] = inputNum;
+					break;
+				}
+			}
+		}
+	}
+}
